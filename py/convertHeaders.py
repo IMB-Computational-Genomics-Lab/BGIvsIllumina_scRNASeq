6cee4a00d174fa4ca3cb4a3fad515c7bdc6ee7dd
Anne Senabouth <a.senabouth@imb.uq.edu.au>
2019-02-22 15:08:00 +1000

    Basic framework of scripts complete

--- py/convertHeaders.py
+++ py/convertHeaders.py
@@ -3,20 +3,25 @@
 import argparse
 import re
 import gzip
+import itertools
+import multiprocessing as mp
 
 # Fastq.gz module by Guillaume Filion
 import gzopen
+
+# Worker function to perform task over list of reads
+def worker(chunk):
+    return len(chunk)
 
 # Retrieves arguments inputted by user
 def parseArguments(args):
     # Retrieve from parser
     input_file = args.input
     output_file = args.output
-    convert_opt = args.convert
     
     # Extract barcode from filename
     sample_index = input_file.split("_")[3]
-    return input_file, output_file, convert_opt, sample_index
+    return input_file, output_file, sample_index
 
 
 # Sets up arguments for user input
@@ -25,18 +30,26 @@ def setupParser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", action = "store", help = "fastq.gz file you want to convert", type = str)
     parser.add_argument("-o", "--output", action = "store", help = "Name of the file you want the converted file.", type = str)
-    parser.add_argument("-c", "--convert", action = "store", help = "Format to convert to - bgi, illumina", choices = ["illumina", "bgi"])
     args = parser.parse_args()
+
     return(args)
 
 
 # Code starts here
 if __name__ == "__main__":
     args = setupParser()
-    input_file, output_file, convert_opt, sample_index = parseArguments(args)
-        
+    input_file, output_file, sample_index = parseArguments(args)
+    
     with gzopen.gzopen(input_file) as input_fastq:
-        with gzip.open(output_file, "wb") as output_fastq:
-            for line in input_fastq:
-                parsed_line = parseLine(line, sample_index = sample_index)
-                output_fastq.write(parsed_line)
+        for chunk in itertools.islice(input_fastq, 1e6):
+            print chunk
+        #worker_pool = mp.Pool(4)
+        #processed = worker_pool.map(worker, input_fastq, chunksize = 1e6)
+        #worker_pool.close()
+        #worker_pool.join()
+  
+        #print processed
+        #with gzip.open(output_file, "wb") as output_fastq:
+        #    for line in input_fastq:
+        #        parsed_line = parseLine(line, sample_index = sample_index)
+        #        output_fastq.write(parsed_line)
